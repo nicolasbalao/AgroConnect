10ee23b1edd640500b8fedc9171d239bbcb00afe
Nicolas Balao <61119970+nicolasbalao@users.noreply.github.com>
2024-03-12 16:08:49 +0100

    feat(software): Make default navigation and users controlles + BaseViewModels and command system (#12)

--- Software/MainWindow.xaml.cs
+++ Software/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Software.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Software
 {
@@ -19,6 +11,9 @@ namespace Software
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
+            ((MainViewModel)DataContext).NavigateCommand.Execute("Employees");
+
         }
     }
 }
