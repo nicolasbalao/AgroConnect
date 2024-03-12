eb78ca088bab27e0f20364ca97d84630bc4f90c4
nicolas balao <nicolasbalao.pro@gmail.com>
2024-03-12 16:08:10 +0100

    feat(software): Make default navigation and users controlles + BaseViewModels and command system

---
+++ Software/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Input;
+using Software.Views;
+
+namespace Software.ViewModels
+{
+    internal class MainViewModel : ViewModelBase 
+    {
+
+        public ICommand NavigateCommand { get; }
+
+        public MainViewModel()
+        {
+            NavigateCommand = new RelayCommand<string>(Navigate);
+        }
+
+        public void Navigate(string section)
+        {
+            switch (section)
+            {
+                case "Sites":
+                    NavigateTo(new SiteView());
+                    break;
+                case "Departments":
+                    NavigateTo(new DepartmentView());
+                    break;
+                case "Employees":
+                    NavigateTo(new EmployeeView());
+                    break;
+                default:
+                    throw new ArgumentException("Invalid destination");
+            }
+
+        }
+        private void NavigateTo(object destination)
+        {
+            ((MainWindow)System.Windows.Application.Current.MainWindow).MainFrame.Content = destination;
+        }
+    }
+}
