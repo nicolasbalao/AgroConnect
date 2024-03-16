0771110d6317c2fa296d92265acb882360855704
nicolas balao <nicolasbalao.pro@gmail.com>
2024-03-16 16:31:16 +0100

    #22 fix(api): Fix custom authorize not working as expected

--- api/Filters/AdminAuthorize.cs
+++ api/Filters/AdminAuthorize.cs
@@ -2,10 +2,11 @@ using System.Text;
 using api.Services;
 using Microsoft.AspNetCore.Mvc.Filters;
 using api.CustomException;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Filters;
 
-public class AdminAuthorize : ActionFilterAttribute
+public class AdminAuthorize : Attribute, IAuthorizationFilter
 {
 
     private readonly IAuthTokenService _authTokenService;
@@ -15,30 +16,27 @@ public class AdminAuthorize : ActionFilterAttribute
         _authTokenService = authTokenService;
     }
 
-    public override void OnActionExecuted(ActionExecutedContext context)
+    public void OnAuthorization(AuthorizationFilterContext context)
     {
+
         string? bearerToken = context.HttpContext.Request.Headers["Authorization"];
 
-        if (bearerToken == null)
+        if (string.IsNullOrEmpty(bearerToken) || !bearerToken.StartsWith("Bearer "))
         {
-            throw new UnauthorizedExecption("Unauthorized");
+            context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
             return;
-
         }
 
-        string token = bearerToken.Replace("Bearer ", "");
 
+        string token = bearerToken.Replace("Bearer ", "");
         if (!IsAuthorized(token))
         {
-            throw new UnauthorizedExecption("Unauthorized");
+            context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
             return;
         }
 
         context.HttpContext.Items["User"] = _authTokenService.GetPayload(token);
-
-        base.OnActionExecuted(context);
     }
-
 
     private bool IsAuthorized(string token)
     {
